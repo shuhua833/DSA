5b887feb8b98fcec1ff2b428b6827f12e11ca385
Shuhua Shrestha <71085603+shuhua833@users.noreply.github.com>
2021-07-16 12:08:10 +0545

    Create Insert at first.c

---
+++ linked lists/Insert at first.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+struct node
+{
+int info;
+struct node *link;
+};
+struct node * start=NULL;
+struct node *createnode()
+{
+struct node *n;
+n=(struct node*)malloc(sizeof(node));
+return(n);
+}
+void insertnode()
+{
+struct node *temp;
+temp=(struct node*)malloc(sizeof(node));
+printf("enter a number\n");
+scanf("%d",&temp->info);
+if(start==NULL)
+{
+start=temp;
+temp->link=NULL;
+}
+else
+{
+temp->link=start;
+start=temp;
+}
+}
+void Display()
+{
+struct node *t;
+t=start;
+while(t!=NULL)
+{
+printf("%d\n",t->info);
+t=t->link;
+}
+}
+int main()
+{
+int cho;
+char ch;
+do{
+printf("1: Insert\n");
+printf("2: Display\n");
+printf("3: Exit\n");
+printf("enter you choice\n");
+scanf("%d",&cho);
+switch(cho)
+{
+case 1:
+printf("case1\n");
+insertnode();
+break;
+case 2:
+Display();
+break;
+case 3:
+exit(0);
+default:
+printf("wrong choice\n");
+}
+printf("do you want to continue Y or y\n");
+fflush(stdin);
+scanf("%c",&ch);
+}while(ch=='Y'||ch=='y');
+getch();
+return 0;
+}
